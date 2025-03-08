79404a1527bdb2d34ff7e37f51f71b649df063e0
Huiseong Lim <105583102+limhuiseong@users.noreply.github.com>
2025-03-08 05:34:18 +0000

    Implement skeleton for kernel_main function

--- kernel.c
+++ kernel.c
@@ -2,9 +2,18 @@
 
 extern char __bss[], __bss_end[], __stack_top[];
 
+void kernel_main(void) {
+    
+}
+
 __attribute__((section(".text.boot")))
 __attribute__((naked))
 void boot(void)
 {
-    
+    __asm__ __volatile__(
+        "mv sp, %[stack_top]\n"
+        "j kernel_main\n"
+        :
+        : [stack_top] "r" (__stack_top)
+    );
 }
