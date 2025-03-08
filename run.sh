197b0e12d2926e2f865f4270adfbf05c1a51e078
Huiseong Lim <105583102+limhuiseong@users.noreply.github.com>
2025-03-08 07:50:08 +0000

    Add lib to compile list

--- run.sh
+++ run.sh
@@ -6,8 +6,10 @@ QEMU=qemu-system-riscv32
 CC=clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib -I./include"
 
+LIB=$(find ./lib -name "*.c")
+
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c
+    kernel.c $LIB
 
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
     -kernel kernel.elf
