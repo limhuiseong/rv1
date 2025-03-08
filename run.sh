c19cdaa1b1027544b2b41095a6851f3ec44d0457
Huiseong Lim <105583102+limhuiseong@users.noreply.github.com>
2025-03-08 07:41:52 +0000

    Add include path to clang flags

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -xue
 QEMU=qemu-system-riscv32
 
 CC=clang
-CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib -I./include"
 
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
     kernel.c
