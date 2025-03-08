18f93d376574b30e7e7d703121d2ffb6414c2f47
Huiseong Lim <105583102+limhuiseong@users.noreply.github.com>
2025-03-08 05:24:40 +0000

    Integrate run.sh and Makefile

--- Makefile
+++
@@ -1,2 +0,0 @@
-CC = clang
-CFLAGS = -std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib
--- run.sh
+++ run.sh
@@ -3,4 +3,11 @@ set -xue
 
 QEMU=qemu-system-riscv32
 
-make
+CC=clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
+
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c
+
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -kernel kernel.elf
