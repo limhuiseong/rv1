67566dfaeb0f022c1feb37c05a837d2e843c970a
Huiseong Lim <limhuiseong.cs@gmail.com>
2025-03-22 13:28:35 +0000

    Add boot message

--- kernel.c
+++ kernel.c
@@ -1,10 +1,15 @@
 #include "stdint.h"
 #include "string.h"
+#include "console.h"
 
 extern char __bss[], __bss_end[], __stack_top[];
 
 void kernel_main(void) {
     memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
+
+    char* boot_message = "\nbooted\n";
+    while (*boot_message)
+        putchar(*boot_message++);
 
     while (1);
 }
