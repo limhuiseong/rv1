d7425718ab4090e805612839654350eb52567f84
Huiseong Lim <limhuiseong.cs@gmail.com>
2025-03-22 13:50:35 +0000

    Add types and related definitions

--- include/stdint.h
+++ include/stdint.h
@@ -1,8 +1,23 @@
 #ifndef _STDINT_H
 #define _STDINT_H
 
-typedef unsigned char   uint8_t;
-typedef unsigned int    uint32_t;
-typedef uint32_t        size_t;
+#define true                    1
+#define false                   0
+#define NULL                    ((void*)0)
+
+#define align_up(value, align)   __builtin_align_up(value, align)
+#define is_aligned(value, align) __builtin_is_aligned(value, align)
+#define offsetof(type, member)   __builtin_offsetof(type, member)
+
+typedef unsigned char           uint8_t;
+typedef unsigned short          uint16_t;
+typedef unsigned int            uint32_t;
+typedef unsigned long long      uint64_t;
+
+typedef uint32_t                size_t;
+typedef uint32_t                paddr_t;
+typedef uint32_t                vaddr_t;
+
+typedef int                     bool;
 
 #endif
