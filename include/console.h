2308b3b56f7d6edb40716d6c25f5903a80a4305a
Huiseong Lim <limhuiseong.cs@gmail.com>
2025-03-22 13:39:28 +0000

    Define variadic macro

--- include/console.h
+++ include/console.h
@@ -3,6 +3,11 @@
 
 #include "sbi.h"
 
+#define va_list     __builtin_va_list
+#define va_start    __builtin_va_start
+#define va_end      __builtin_va_end
+#define va_arg      __builtin_va_arg
+
 struct sbiret putchar(char c);
 void printf(const char *fmt, ...);
 
