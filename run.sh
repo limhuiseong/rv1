11325e034d2769a660b0fc9f661db8a3189a121d
Huiseong Lim <limhuiseong.cs@gmail.com>
2025-03-22 05:30:35 +0000

    Add arch to compile list

--- run.sh
+++ run.sh
@@ -7,9 +7,10 @@ CC=clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib -I./include"
 
 LIB=$(find ./lib -name "*.c")
+ARCH=$(find ./arch/riscv -name "*.c")
 
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c $LIB
+    kernel.c $LIB $ARCH
 
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
     -kernel kernel.elf
