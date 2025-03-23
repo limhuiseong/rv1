34b9ee64185e411563b07f3ea87c9c10ccbecd79
Huiseong Lim <limhuiseong.cs@gmail.com>
2025-03-23 07:30:05 +0000

    Define a kernel panic handling inline function

---
+++ include/panic.h
@@ -0,0 +1,10 @@
+#ifndef _PANIC_H
+#define _PANIC_H
+
+#define PANIC(fmt, ...)                                                         \
+    do {                                                                        \                             
+        printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);   \
+        while (1);                                                              \
+    } while (0)
+
+#endif
