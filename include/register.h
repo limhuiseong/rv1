532620411f2a705ffee5bd7e63549625c04b2ab2
Huiseong Lim <limhuiseong.cs@gmail.com>
2025-03-23 07:47:34 +0000

    Define CSR R/W inline functions

---
+++ include/register.h
@@ -0,0 +1,19 @@
+#ifndef _REGISTER_H
+#define _REGISTER_H
+
+#include "stdint.h"
+
+#define READ_CSR(reg)                                               \
+    ({                                                              \                                 
+        unsigned long __tmp;                                        \
+        __asm__ __volatile__("csrr %0, " #reg : "=r"(__tmp));       \
+        __tmp;                                                      \
+    })
+
+#define WRITE_CSR(reg, value)                                       \
+    do {                                                            \                           
+        uint32_t __tmp = (value);                                   \      
+        __asm__ __volatile__("csrw " #reg ", %0" : : "r"(__tmp));   \
+    } while (0)
+
+#endif
