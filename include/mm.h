6bc074f06b376addc0e6cc401eb477f290e5a3da
Huiseong Lim <limhuiseong.cs@gmail.com>
2025-03-26 11:08:39 +0000

    Move declarations to header file

--- include/mm.h
+++ include/mm.h
@@ -2,8 +2,14 @@
 #define _MM_H
 
 #include "stdint.h"
+#include "panic.h"
+#include "string.h"
 
 #define PAGE_SIZE 4096
+
+extern char __free_ram[], __free_ram_end[];
+
+static paddr_t next_paddr = (paddr_t)__free_ram;
 
 paddr_t palloc(uint32_t n);
 paddr_t pcalloc(uint32_t n);
