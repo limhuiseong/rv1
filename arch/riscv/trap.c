5753d47b0d4efcbafe157c2adcea746d38a35f8f
Huiseong Lim <limhuiseong.cs@gmail.com>
2025-03-26 10:40:57 +0000

    Rename a function and add to-do list

--- arch/riscv/trap.c
+++ arch/riscv/trap.c
@@ -2,11 +2,12 @@
 #include "register.h"
 #include "panic.h"
 
-void handle_unexpected_trap(struct trap_frame* tf)
+void handle_trap(struct trap_frame* tf)
 {
+    // TODO: Implement trap branch and each detail
     uint32_t scause = READ_CSR(scause);
     uint32_t stval = READ_CSR(stval);
     uint32_t user_pc = READ_CSR(sepc);
 
-    PANIC("unexpected trap: scause=0x%08x, stval=0x%08x, user_pc=0x%08x", scause, stval, user_pc);
+    PANIC("Trap: scause=0x%08x, stval=0x%08x, user_pc=0x%08x", scause, stval, user_pc);
 }
