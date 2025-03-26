a88ffc10d4d75e0143ac24f7c515b5480b84a54b
Huiseong Lim <limhuiseong.cs@gmail.com>
2025-03-26 10:30:38 +0000

    Rename a trap handling function

--- arch/riscv/trap.c
+++ arch/riscv/trap.c
@@ -2,11 +2,11 @@
 #include "register.h"
 #include "panic.h"
 
-void handle_trap(struct trap_frame* tf)
+void handle_unexpected_trap(struct trap_frame* tf)
 {
     uint32_t scause = READ_CSR(scause);
     uint32_t stval = READ_CSR(stval);
     uint32_t user_pc = READ_CSR(sepc);
 
-    PANIC("Trap: scause=0x%08x, stval=0x%08x, user_pc=0x%08x", scause, stval, user_pc);
+    PANIC("unexpected trap: scause=0x%08x, stval=0x%08x, user_pc=0x%08x", scause, stval, user_pc);
 }
